a43e75f7c84cf7d3d524068bfc04d924e567981d
srinivasteja18 <85738509+srinivasteja18@users.noreply.github.com>
2021-06-28 10:02:22 +0530

    Add files via upload

---
+++ JsPage.js
@@ -0,0 +1,14 @@
+﻿window.addEventListener('keydown', function (e) {
+    console.log(e);
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    var key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add("playing");
+});
+
+window.addEventListener('keyup', function (e) {
+    var key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    key.classList.remove("playing");
+});
