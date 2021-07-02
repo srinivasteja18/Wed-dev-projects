df24c4b631bde0e6e79aa6072a16058688084ae7
srinivasteja18 <85738509+srinivasteja18@users.noreply.github.com>
2021-07-02 12:26:49 +0530

    Add files via upload

---
+++ Image Gallery/JavaScript.js
@@ -0,0 +1,8 @@
+﻿
+const panels = document.querySelectorAll(`.panel`);
+console.log(panels);
+function helper() {
+    this.classList.toggle('open');
+}
+
+panels.forEach(temp => temp.addEventListener('click', helper));
