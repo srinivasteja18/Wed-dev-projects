87a2411a67f36626767354a1b4e9dac60a7fb208
srinivasteja18 <85738509+srinivasteja18@users.noreply.github.com>
2021-07-03 12:17:07 +0530

    Add files via upload

---
+++ Seacrh Suggestion/JavaScript.js
@@ -0,0 +1,32 @@
+﻿
+const data = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+const arr=[]
+fetch(data).then(cur => cur.json())
+    .then(val => arr.push(...val));
+//console.log(arr);
+function findMatch(word,arr) {
+    return arr.filter(cur => {
+        const regex = new RegExp(word, 'gi');
+        return cur.city.match(regex) || cur.state.match(regex);
+    });
+}
+
+function displayMatch() {
+    const cur = findMatch(this.value, arr);
+    const text = cur.map(val => {
+        const regex = new RegExp(this.value, 'gi');
+        const cityName = val.city.replace(regex, `<span class="back">${this.value}</span>`);
+        const stateName = val.state.replace(regex, `<span class="back">${this.value}</span>`);
+        return `
+                <li><span class="name"> ${cityName}, ${stateName}</span></li>
+            `
+    }).join('');
+    lists.innerHTML = text;
+}
+
+const inputs = document.querySelector('.search');
+const lists = document.querySelector('.suggestions');
+
+inputs.addEventListener('change', displayMatch);
+inputs.addEventListener('keyup', displayMatch);
